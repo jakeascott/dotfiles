00980ddac5d2b18e575203f7c4ee2b7d931dc143
Jake <scottj@sou.edu>
2019-02-08 14:16:23 -0800

    Ready for install

--- arch-config/update-mirrors.sh
+++ arch-config/update-mirrors.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&ip_version=4&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
+curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&ip_version=4&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^## U/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
