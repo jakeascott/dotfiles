53d54c656df1deb8122fe1da1e6eefa90b48825b
Jake <scottj@sou.edu>
2019-03-14 12:36:37 -0700

    updating arch-config scripts

--- arch-config/update-mirrors.sh
+++ arch-config/update-mirrors.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# run as root
+# Grabs top 5 arch mirrors from and puts them in /etc/pacman.d/mirrorlist
+
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+
 curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&ip_version=4&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^## U/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
