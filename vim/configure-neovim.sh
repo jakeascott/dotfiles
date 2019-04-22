028adaea9b95f49b0d595197443d59a274c6be0e
Jake Scott <scottj@sou.edu>
2019-04-22 08:33:16 -0700

    added neovim install script

--- vim/configure-neovim.sh
+++ vim/configure-neovim.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+if dpkg --get-selections | grep -q "^neovim" > /dev/null; then
+    sudo apt install neovim
+fi
+
+# Install vim config
 [ ! -d ~/.config/nvim] && mkdir ~/.config/nvim 
 cp vimrc ~/.config/nvim/init.vim
 echo 'Installed ~/.config/nvim/init.vim'
 
-echo 'Installing vim-plug'
 # Get vim-plug
+echo 'Installing vim-plug'
 curl -fLo ~/.local/share/nvim/autoload/plug.vim --create-dirs 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir ~/.local/share/nvim/plugged
 
