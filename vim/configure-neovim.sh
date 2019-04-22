c4c8847874c529eda14253b4c5b3c10d8b4a50c2
Jake Scott <scottj@sou.edu>
2019-04-22 08:51:25 -0700

    fixed neovim install script bugs

--- vim/configure-neovim.sh
+++ vim/configure-neovim.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-if dpkg --get-selections | grep -q "^neovim" > /dev/null; then
+if ! dpkg --get-selections | grep -q "^neovim" > /dev/null; then
     sudo apt install neovim
 fi
 
 # Install vim config
-[ ! -d ~/.config/nvim] && mkdir ~/.config/nvim 
+echo 'Installing init.vim'
+[ ! -d ~/.config/nvim ] && mkdir ~/.config/nvim 
 cp vimrc ~/.config/nvim/init.vim
-echo 'Installed ~/.config/nvim/init.vim'
 
 # Get vim-plug
 echo 'Installing vim-plug'
-curl -fLo ~/.local/share/nvim/autoload/plug.vim --create-dirs 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-mkdir ~/.local/share/nvim/plugged
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+mkdir ~/.local/share/nvim/site/plugged
 
 echo 'Done. Open nvim and run :PlugInstall to install plugins'
