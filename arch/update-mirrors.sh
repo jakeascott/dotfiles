e2d46cc306030432676316a8e2923ba8fbe720e4
Jake Scott <scottj@sou.edu>
2019-06-27 14:59:04 -0700

    update arch config

--- arch/update-mirrors.sh
+++ arch/update-mirrors.sh
@@ -9,4 +9,6 @@ cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 echo 'Pulling current mirrorlist from archlinux.org/mirrorlist...'
 curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&ip_version=4&ip_version=6&uuse_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^## U/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 
-pacman -Syy && echo 'pacman mirrorlist updated!' && cat /etc/pacman.d/mirrorlist
+pacman -Syy && echo 'pacman mirrorlist updated.'
+pacman-key --refresh-keys && echo 'pacman keyring updated.'
+cat /etc/pacman.d/mirrorlist
