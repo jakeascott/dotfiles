4c72466e20a514b07d02377b84e35d0137f5ccf7
Jake Scott <scottj@sou.edu>
2019-07-03 09:13:23 -0700

    added install-i3.sh

---
+++ arch/install-i3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Run as normal user
+
+echo 'Be sure to install appropriate graphics driver.'
+sudo pacman -Sy --noconfirm i3-gaps i3blocks i3lock dmenu
+
+WORKDIR=$(pwd | sed 's/dotfiles.*/dotfiles/')
+LOCAL="$HOME/.local"
+CONFIG="$HOME/.config"
+
+# Install configs
+echo 'Installing i3 configs...'
+mkdir -vp $CONFIG/i3
+cp $WORKDIR/i3/config $CONFIG/config
+cp -r $WORKDIR/i3/i3blocks $CONFG/i3blocks
