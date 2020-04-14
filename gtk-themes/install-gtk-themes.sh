cec8b72ed1803501e5d8f49dd57abb72e0ef4776
Jake Scott <scottj@sou.edu>
2020-04-14 12:19:44 -0700

    removed gtk theme

--- gtk-themes/install-gtk-themes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-WORKDIR=$(find $HOME -name dotfiles)
-
-sudo cp -r $WORKDIR/gtk-themes/jakes-gruvbox-gtk /usr/share/themes/
-sudo cp -r $WORKDIR/gtk-themes/jakes-gruvbox-icons /usr/share/icons/
-
-echo 'Copied GTK themes to /usr/share.'
