3331ea1b3480aa7a374d640258139dd5ec2a6249
Jake Scott <scottj@sou.edu>
2020-09-16 15:36:12 -0700

    udate

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,15 +1,59 @@
+(require 'package)
+
+;; Package manager repos
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+;; Ensure use-package is installed
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+
+;; enable evil
+(use-package evil
+  :ensure t
+  :init
+  (evil-mode 1))
+
+;; Enable doom-one theme
+(use-package doom-themes
+  :config
+  (setq doom-themes-enable-bold t
+	doom-themes-enable-italic t)
+  (load-theme 'doom-one t)
+
+  (doom-themes-visual-bell-config)
+  (doom-themes-org-config))
+
+;; Python
+(use-package elpy
+  :ensure t
+  :init
+  (elpy-enable))
+
+;; Start emacs window maximized
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; Clean up some defaults
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(global-display-line-numbers-mode)
+(setq inhibit-startup-screen t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(custom-enabled-themes '(misterioso))
- '(show-paren-mode t))
+ '(package-selected-packages '(elpy use-package evil doom-themes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 102 :width normal)))))
+ )
